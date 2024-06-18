6fd2bbc7ae0d9078e92a3baa0e6f4587675c96d9
riddhi <159906724+riddhidomadiya@users.noreply.github.com>
2024-06-18 17:07:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,18 @@ app.use(express.json())
 const port = process.env.PORT || 80
 require("./db/db")
 
+const User = require("./user")
+
+app.post('/user', async (req, res) => {
+    const user = await User.create(req.body)
+    res.send(user)
+});
+
+app.get('/user', async (req, res) => {
+	const user = await User.find({})
+    res.send(user)
+});
+
 app.get('/', async (req, res) => {
 	res.send("backend api run on server...");
 });
